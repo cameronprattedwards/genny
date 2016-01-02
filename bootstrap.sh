e291788650a026778ce108313b4735a4235cde2a
Cameron Edwards <cameron@hioscar.com>
2016-01-02 17:36:41 -0500

    Add prod command

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,3 @@
-sudo apt-get update
-sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y
 sudo apt-add-repository -y ppa:chris-lea/node.js
 sudo apt-get update
@@ -13,12 +11,4 @@ sudo apt-get install -y build-essential libssl-dev curl git-core
 sudo npm install -g --unsafe-perm node-inspector
 sudo npm install -g nodemon webpack-dev-server
 
-sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password rootpass'
-sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password rootpass'
 sudo apt-get install -y mysql-server mysql-client
-# cd /vagrant/App/Server
-# sudo forever start main.js
-mysql -u root -p"rootpass" -e "create database genny DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
-
-cd /vagrant/migrations
-mysql -u root -p"rootpass" < 1.sql
