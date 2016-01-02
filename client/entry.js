7bf58452e1201b7c09e575a073be9fcb464b925a
Cameron Edwards <cameron@hioscar.com>
2016-01-01 19:15:19 -0500

    Add babel-runtime

--- client/entry.js
+++ client/entry.js
@@ -2,6 +2,7 @@ console.log(new Date().toString());
 
 process.on('uncaughtException', function(err) {
   console.log(`Caught exception: ${err}`);
+  console.log(err.stack);
 });
 
 require('babel-core/register');
