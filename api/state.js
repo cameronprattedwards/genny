cbc55d6998c08b06106317e33b017271cf22e3fa
cameronprattedwards <cameron@hioscar.com>
2016-01-13 18:40:55 -0500

    Fix email issue

--- api/state.js
+++ api/state.js
@@ -32,7 +32,7 @@ export const getUserState = async function getUserState(token) {
 			token,
 			login,
 			repoName,
-			email,
+			email: email.email,
 			avatar: avatar_url,  // eslint-disable-line camelcase
 			currentStep: db.modules[db.moduleOrder[0]].steps[0],
 		},
