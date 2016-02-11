e3ea578f0760eb0e7c2d885385da9d8c36d24e08
cameronprattedwards <cameron@hioscar.com>
2016-02-11 17:40:45 -0500

    Remove hot loading from production config

--- production.config.js
+++ production.config.js
@@ -1,6 +1,35 @@
 var webpack = require('webpack');
-var config = require('./webpack.config');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var cssLoaders = ExtractTextPlugin.extract('style-loader', 'css-loader?modules');
+var plugins = [
+  new ExtractTextPlugin('bundle.css', {allChunks: true}),
+  new webpack.optimize.UglifyJsPlugin({minimize: true})
+];
 
-config.plugins = config.plugins.concat(new webpack.optimize.UglifyJsPlugin({minimize: true}));
-
-module.exports = config;
+module.exports = {
+  entry: [
+    './client/index.jsx'
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'react-hot-loader!babel?presets[]=react,presets[]=es2015'
+      },
+      { 
+        test: /\.css$/, 
+        exclude: /node_modules/,
+        loader: cssLoaders
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.css']
+  },
+  output: {
+    path: __dirname + '/client/public/dist',
+    filename: 'bundle.js'
+  },
+  plugins: plugins
+};
