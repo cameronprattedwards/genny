282792309696f8553f764d31b40d5939a130fb60
cameronprattedwards <cameron@hioscar.com>
2016-02-11 16:48:27 -0500

    Make spinner giant

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -8,7 +8,7 @@ import styles from './Home.css';
 import {FIRST_PANE, OPEN_YOUR_TERMINAL, setupUrl} from './setup/metadata';
 import {SUCCESS} from '../../domain/constants';
 
-import {Spinner} from '../../utils/components/Spinner';
+import {Spinner, SpinnerSizes} from '../../utils/components/Spinner';
 import {Button} from '../../utils/components/Button';
 import {AuthorizeContainer} from './Authorize';
 
@@ -16,35 +16,21 @@ const loginPath = reversePath(Paths.LOGIN, false);
 
 export const Home = React.createClass({
 	render() {
-		let callToAction = null;
+		let callToAction = getCallToAction(this.props);
 
-		if (this.props.loading) {
-			callToAction = <div className={styles.spinner}><Spinner /></div>;
-		} else if (this.props.token) {
-			if (this.props.setupFinished) {
-				callToAction = (
-					<p>
-						<Button component={Link} to={`/step/${this.props.currentStep}`}>Click here</Button>
-						{' '}to pick up where you left off.
-					</p>
-				);
-			} else {
-				callToAction = (
-					<p>
-						<Button component={Link} to={setupUrl(OPEN_YOUR_TERMINAL)}>Click here</Button>
-						{' '}to get started.
-					</p>
-				);
-			}
-		} else {
-			callToAction = (
+		let content = null;
+
+		if (!this.props.loading) {
+			content = (
 				<div>
+					<h1>School of Haxx is a free HTML tutorial.</h1>
+					<p>We'll teach you the basics of HTML, and you'll be building your own webpages in no time. </p>
 					<p>
-						<Button component={Link} to={setupUrl(FIRST_PANE)}>Click here</Button> to get started. 
-						(Don't worry - it's free.) 
+						Plus, we'll do it using tools that real programmers use - like <strong>Sublime Text</strong>, 
+						<strong> the terminal</strong> and <strong>git</strong>.
 					</p>
 					<p>
-						Or <AuthorizeContainer>log in</AuthorizeContainer> if you already have an account.
+						It's simple as pie. In a couple minutes you'll be writing webpages like a pro.
 					</p>
 				</div>
 			);
@@ -52,30 +38,46 @@ export const Home = React.createClass({
 
 		return (
 			<div className={styles.home}>
-				<h1>School of Haxx is a free HTML tutorial.</h1>
-				<p>We'll teach you the basics of HTML, and you'll be building your own webpages in no time. </p>
-				<p>
-					Plus, we'll do it using tools that real programmers use - like <strong>Sublime Text</strong>, 
-					<strong> the terminal</strong> and <strong>git</strong>.
-				</p>
-				<p>
-					It's simple as pie. In a couple minutes you'll be writing webpages like a pro.
-				</p>
+				{content}
 				{callToAction}
 			</div>
 		);
 	},
-
-	openLoginWindow(event) {
-		event.preventDefault();
-		if (!this.props.token) {
-			const child = window.open(loginPath, '', 'width=500,height=500');
-			this.props.setChildWindow(child);
-		} else {
-			this.props.history.pushState(null, `/step/${this.props.currentStep}`);
-		}
-	},
 });
+
+function getCallToAction({loading, setupFinished, currentStep, token}) {
+	if (loading) {
+		return <div className={styles.spinner}><Spinner size={SpinnerSizes.GIANT} /></div>;
+	} else if (token) {
+		if (setupFinished) {
+			return (
+				<p>
+					<Button component={Link} to={`/step/${currentStep}`}>Click here</Button>
+					{' '}to pick up where you left off.
+				</p>
+			);
+		}
+
+		return (
+			<p>
+				<Button component={Link} to={setupUrl(OPEN_YOUR_TERMINAL)}>Click here</Button>
+				{' '}to get started.
+			</p>
+		);
+	}
+
+	return (
+		<div>
+			<p>
+				<Button component={Link} to={setupUrl(FIRST_PANE)}>Click here</Button> to get started. 
+				(Don't worry - it's free.) 
+			</p>
+			<p>
+				Or <AuthorizeContainer>log in</AuthorizeContainer> if you already have an account.
+			</p>
+		</div>
+	);
+}
 
 function mapStateToProps(state) {
 	return {
--- utils/components/Spinner.jsx
+++ utils/components/Spinner.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import styles from './Spinner.css';
+import cx from 'classnames';
+
+export const SpinnerSizes = {
+	SMALL: styles.small,
+	GIANT: styles.giant,
+};
 
 export const Spinner = React.createClass({
+	getDefaultProps() {
+		return {
+			size: SpinnerSizes.SMALL,
+		};
+	},
+
 	render() {
-		return <i className="fa fa-spinner fa-spin" />;
+		const classNames = cx(
+			'fa',
+			'fa-spinner',
+			'fa-spin',
+			this.props.size,
+		);
+
+		return <i className={classNames} />;
 	},
 });
